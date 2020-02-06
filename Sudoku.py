17f98375f76c1fe66904fdd6d4a665e0308db24f
HanssenJarrah <55371005+HanssenJarrah@users.noreply.github.com>
2020-02-06 19:58:51 +1000

    Optimised the validation of partial solutions

--- Sudoku.py
+++ Sudoku.py
@@ -2,7 +2,8 @@
 
 EMPTY = 0
 
-# The allegedly 'hardest' sudoku problem
+"""
+# The allegedly 'hardest' sudoku problem:
 sudoku_problem = [
     [8, 0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 3, 6, 0, 0, 0, 0, 0],
@@ -13,8 +14,8 @@ sudoku_problem = [
     [0, 0, 1, 0, 0, 0, 0, 6, 8],
     [0, 0, 8, 5, 0, 0, 0, 1, 0],
     [0, 9, 0, 0, 0, 0, 4, 0, 0]]
-
 """
+
 sudoku_problem = [
     [5, 3, 0, 0, 7, 0, 0, 0, 0],
     [6, 0, 0, 1, 9, 5, 0, 0, 0],
@@ -35,7 +36,7 @@ sudoku_solution = [
     [7, 1, 3, 9, 2, 4, 8, 5, 6],
     [9, 6, 1, 5, 3, 7, 2, 8, 4],
     [2, 8, 7, 4, 1, 9, 6, 3, 5],
-    [3, 4, 5, 2, 8, 6, 1, 7, 9]]"""
+    [3, 4, 5, 2, 8, 6, 1, 7, 9]]
 
 
 def is_complete(board):
@@ -59,39 +60,39 @@ def find_empty_entry(board):
     return False
 
 
-def rows_valid(board):
+def row_valid(board, row):
+    if duplicates(board[row]):
+        return False
+    return True
+
+
+def column_valid(board, column):
+    col_arr = []
     for row in board:
-        if duplicates(row):
-            return False
-    return True
-
-
-def columns_valid(board):
-    for j in range(len(board[0])):
-        col_arr = [board[i][j] for i in range(len(board))]
-        if duplicates(col_arr):
-            return False
-    return True
-
-
-def blocks_valid(board):
-    for i in range(0, 9, 3):
-        for j in range(0, 9, 3):
-            block_arr = []
-            for k in range(3):
-                for l in range(3):
-                    block_arr.append(board[i + k][j + l])
-            if duplicates(block_arr):
-                return False
-    return True
-
-
-def currently_valid(board):
-    if not rows_valid(board):
+        col_arr.append(row[column])
+    if duplicates(col_arr):
         return False
-    if not columns_valid(board):
+    return True
+
+
+def block_valid(board, row, column):
+    block_arr = []
+    block_r = (row // 3) * 3
+    block_c = (column // 3) * 3
+    for i in range(block_r, block_r + 3):
+        for j in range(block_c, block_c + 3):
+            block_arr.append(board[i][j])
+    if duplicates(block_arr):
         return False
-    if not blocks_valid(board):
+    return True
+
+
+def currently_valid(board, row, column):
+    if not row_valid(board, row):
+        return False
+    if not column_valid(board, column):
+        return False
+    if not block_valid(board, row, column):
         return False
     return True
 
@@ -103,7 +104,7 @@ def solve_sudoku(board):
     r, c = find_empty_entry(board)
     for val in range(1, 10):
         board[r][c] = val
-        if currently_valid(board):
+        if currently_valid(board, r, c):
             result = solve_sudoku(board)
             if is_complete(result):
                 return result
@@ -129,8 +130,6 @@ def main():
 
     # print("Is the solution the correct solution?")
     # print(solution == sudoku_solution)
-    # print("Is the solution valid?")
-    # print(currently_valid(sudoku_problem))
 
 
 if __name__ == "__main__":
